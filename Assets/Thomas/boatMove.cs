88710a506fdfe64a1b703e005dfb4abeac4d2394
Thomas <1707235@mukilteo.wednet.edu>
2023-01-02 14:08:38 -0800

    another small push.

--- Assets/Thomas/boatMove.cs
+++ Assets/Thomas/boatMove.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class boatMove : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
